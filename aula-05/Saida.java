ce87c3a36c0b107075f2fb755e2e06ba57ca55d9
Gilvan André Gobbato <gilvangobbato@gmail.com>
2023-08-04 08:47:17 -0300

    Aula 5

---
+++ aula-05/Saida.java
@@ -0,0 +1,17 @@
+public class Saida {
+    public static void main(String[] args){
+        //println
+        System.out.println("Ola dev!");
+        System.out.println("Nova linha com System.out.println()");
+        System.out.println("");
+        
+        //print
+        System.out.print("Ola dev!\n");
+        System.out.print("Nova linha com System.out.println()\n\n\n");
+        
+        //printf
+        String nome = "Gilvan";
+        int ano = 2023;
+        System.out.printf("Dev nome: %s ano %d.\n", nome, ano);
+    }
+}
