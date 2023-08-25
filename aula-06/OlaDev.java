fd32868e84deb75662749b44247af3017baebf55
Gilvan André Gobbato <gilvangobbato@gmail.com>
2023-08-25 14:04:02 -0300

    Variaveis.java

--- aula-06/OlaDev.java
+++
@@ -1,6 +0,0 @@
-public class OlaDev{
-    public static void main(String[] args){
-        String nome = args[0];
-        System.out.println("Olá Dev!" + nome);
-    }
-}
---
+++ aula-06/Variaveis.java
@@ -0,0 +1,31 @@
+/**
+* Classe para explicação de variáveis e suas regras
+* @author Gilvan
+* @version 1.0
+*/
+public class Variaveis{
+    
+    final static String BEM_VINDO = "Bem vindo ";
+    
+    /**
+    * Metodo main para execução
+    */
+    public static void main(String[] args){
+        String nome = "Gilvan";
+        System.out.println(BEM_VINDO + nome);
+        
+        String nome1 = "";
+        
+        /*
+        {
+            //nome = "Fulano";
+            System.out.println(BEM_VINDO + nome);
+            
+            nome1 = "G2";
+        }
+        */
+        System.out.println(nome1);    
+        
+    }
+    
+}
